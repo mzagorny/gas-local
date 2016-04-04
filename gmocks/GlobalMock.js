424bb1dd0062d830ebc09b963f31749ea08ebff3
Mikhail Zagorny <mikhail.zagorny@gmail.com>
2016-04-04 10:05:36 +0300

    GlobalMock updated with dateFormat

--- gmocks/GlobalMock.js
+++ gmocks/GlobalMock.js
@@ -1,12 +1,18 @@
+var util = require('util');
+var formatDate = require('date-format');
+
 module.exports = {
   Logger: {
     log: function(obj) {
-      console.log(obj);
+      console.log.apply(console, arguments);
 				}
   },
   Utilities: {
     formatString: function(format, etc) {
       return util.format.apply(util, arguments);
+				},
+    formatDate: function(date, tz, format) {
+      return formatDate(format, date);
 				}
   }
 };
