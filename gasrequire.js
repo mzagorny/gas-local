29521850fba45d793ab6e28ac6aee7194eed7f04
Mikhail Zagorny <mikhail.zagorny@gmail.com>
2016-07-22 09:12:02 +0300

    doc added

--- gasrequire.js
+++ gasrequire.js
@@ -5,14 +5,21 @@ var path = require('path');
 var util = require('util');
 var debug = util.debuglog('gas-local:require');
 
-function gasrequire(libPath, globalObject) {
+/**
+ * 
+ * 
+ * @param {String} path to folder with downloaded app scripts
+ * @param {Object} global to pass to module. globalMock will be passed if not specified
+ * @returns module
+ */
+function gasrequire(folderPath, globalObject) {
   if (!globalObject){
     debug('no globalObject passed. use default mock');
     globalObject=require('./globalMock');    
   }
   
-  debug('loading from folder: %s',libPath)
-  var files = fs.readdirSync(libPath);
+  debug('loading from folder: %s',folderPath)
+  var files = fs.readdirSync(folderPath);
   var gsFiles = files.filter(function(f) {
     var ext = path.extname(f);
     return ext == '.js';
@@ -22,7 +29,7 @@ function gasrequire(libPath, globalObject) {
  
   for (var i = 0; i < gsFiles.length; i++) {
     var fname = gsFiles[i];
-    var fpath = path.join(libPath, fname);
+    var fpath = path.join(folderPath, fname);
     debug('loading file: %s...',fpath);
 
     var code = fs.readFileSync(fpath);
