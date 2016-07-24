d659b1c39fa4da6903c2045c6ed8699ab1082418
Mikhail Zagorny <mikhail.zagorny@gmail.com>
2016-07-24 16:23:36 +0300

    added email quota call

--- test/src/Utils.js
+++ test/src/Utils.js
@@ -1,18 +1,32 @@
+/**
+Utils to run google services
+and to mock them to run locally 
+*/
 var Utils = {
   /**
    * Get the active user (email)
    * NOTE: When first use then authorization window will appear
    * @returns {String} email
    */
-  GetActiveUser: function () {
+  getActiveUser: function () {
     var email = Session.getActiveUser().getEmail();
     return email;
+  },
+  getRemainingEmailQuota: function () {
+    return MailApp.getRemainingDailyQuota();
   }
 }
 
 /**
  * to run in cloud
  */
-function _run_GetActiveUser() {
-  Logger.log(Utils.GetActiveUser());
+function _run_getActiveUser() {
+  Logger.log("current user:%s", Utils.getActiveUser());
+}
+
+/**
+ * to run in cloud
+ */
+function _run_getRemainingEmailQuota() {
+  Logger.log("remaining email quota:%s", Utils.getRemainingEmailQuota());
 }
