1de493f26d9cb8b524760747737e679281039a17
Mikhail Zagorny <mikhail.zagorny@gmail.com>
2016-07-25 10:07:51 +0300

    test mock of Logger

--- test/gasrequire.test.js
+++ test/gasrequire.test.js
@@ -4,28 +4,36 @@
 var assert = require('assert');
 var gas = require('.././index.js');
 
-describe('library require', function () {
-   var m = gas.require('./test/src');
+describe('library loading', function () {
+  var m = gas.require('./test/src');
 
-   it('content is not loaded to global', function () {
-      assert(typeof x==='undefined');
-      assert(typeof setX==='undefined');
-   })
+  it('content is not loaded to global', function () {
+    assert(typeof x === 'undefined');
+    assert(typeof setX === 'undefined');
+  })
 
-   it('content from all files is accessible', function () {
-      assert(m.hasOwnProperty('x'));
-      assert(m.hasOwnProperty('setX'));
-   })
-   
-   it('variable initialize correct', function () {
-      assert(m.x == 5);
-   })
+  it('content from all files is accessible', function () {
+    assert(m.hasOwnProperty('x'));
+    assert(m.hasOwnProperty('setX'));
+    assert(m.hasOwnProperty('Utils'));
+  })
 
-   it('function updates variable from other file', function () {
-      m.setX(7);
-      assert(m.x == 7);
-   })
+  it('variable initialize correct', function () {
+    assert(m.x == 5);
+  })
+
+  it('function updates variable from other file', function () {
+    m.setX(7);
+    assert(m.x == 7);
+  })
 });
 
-describe('advanced mock of services', function () {
+describe('default mock of services', function () {
+  var m = gas.require('./test/src');
+  it('Logger is mocked', function () {
+    m.Utils.logCurrentDateTime();
+  })
+});
+
+describe('custom mock of services', function () {
 });
