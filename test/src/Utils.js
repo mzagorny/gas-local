9bec01f012f7396e600d0c4eeaef4b43eae45143
Mikhail Zagorny <mikhail.zagorny@gmail.com>
2016-07-25 09:59:16 +0300

    Utils refactor Logger call added

--- test/src/Utils.js
+++ test/src/Utils.js
@@ -1,32 +1,31 @@
 /**
-Utils to run google services
-and to mock them to run locally 
-*/
-var Utils = {
-  /**
-   * Get the active user (email)
-   * NOTE: When first use then authorization window will appear
-   * @returns {String} email
-   */
+ * Utils to play with Google Services
+ */
+var Utils = {  
   getActiveUser: function () {
     var email = Session.getActiveUser().getEmail();
     return email;
   },
   getRemainingEmailQuota: function () {
     return MailApp.getRemainingDailyQuota();
+  },
+  logCurrentDateTime: function () {
+    Logger.log("Current datetime: %s", new Date());
   }
 }
 
 /**
- * to run in cloud
+ * triggers to run in cloud
  */
+
+function _run_logCurrentDateTime() {
+  Utils.logCurrentDateTime();
+}
+
 function _run_getActiveUser() {
   Logger.log("current user:%s", Utils.getActiveUser());
 }
 
-/**
- * to run in cloud
- */
 function _run_getRemainingEmailQuota() {
   Logger.log("remaining email quota:%s", Utils.getRemainingEmailQuota());
 }
